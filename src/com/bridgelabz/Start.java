d8d302eff5bad248ad1900f3401254ad9e72bcad
sanjana8100 <sanjanams812@gmail.com>
2023-03-03 15:10:12 +0530

    added: Check if employee present or absent

--- src/com/bridgelabz/Start.java
+++ src/com/bridgelabz/Start.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
 public class Start {
+
+    public void CheckEmpPresentOrNot(){
+        System.out.println("Checking whether the Employee is Present or Absent...");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck==1.0)
+            System.out.println("Employee is PRESENT.");
+        else
+            System.out.println("Employee is ABSENT.");
+    }
     public static void main(String[] main){
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        Start obj= new Start();
+        obj.CheckEmpPresentOrNot();
     }
 }
