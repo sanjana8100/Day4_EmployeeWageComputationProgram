bb0047596af6b074f2be486a4c87dbddf2a76742
sanjana8100 <sanjanams812@gmail.com>
2023-03-03 15:31:29 +0530

    added: Calculate Employee Daily Wage

--- src/com/bridgelabz/Start.java
+++ src/com/bridgelabz/Start.java
@@ -2,17 +2,32 @@ package com.bridgelabz;
 
 public class Start {
 
-    public void CheckEmpPresentOrNot(){
+    public double CheckEmpPresentOrNot(){
         System.out.println("Checking whether the Employee is Present or Absent...");
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if(empCheck==1.0)
             System.out.println("Employee is PRESENT.");
         else
             System.out.println("Employee is ABSENT.");
+        return empCheck;
+    }
+
+    public int CalculateDailyEmpWage(double EmpPresent){
+        int empHours,empWage,empWagePerHour=20;
+        if(EmpPresent==1.0)
+            empHours = 8;
+        else
+            empHours=0;
+        empWage = empHours * empWagePerHour;
+        System.out.println("Employee Hours: "+empHours);
+        System.out.println("Employee Daily Wage: "+empWage);
+        return empWage;
     }
     public static void main(String[] main){
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         Start obj= new Start();
-        obj.CheckEmpPresentOrNot();
+        double empPresent = obj.CheckEmpPresentOrNot();
+        int empDailyWage= obj.CalculateDailyEmpWage(empPresent);
+
     }
 }
