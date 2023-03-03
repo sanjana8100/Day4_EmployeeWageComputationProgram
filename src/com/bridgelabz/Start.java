78b9c3bceb166c749da98c6cf790c9c4add2d16d
sanjana8100 <sanjanams812@gmail.com>
2023-03-03 14:42:56 +0530

    added: Start

---
+++ src/com/bridgelabz/Start.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Start {
+    public static void main(String[] main){
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
